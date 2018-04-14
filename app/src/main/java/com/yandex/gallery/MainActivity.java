dd55b2b0b159fc7f494dddd8439ee9466c5bcf47
Vladislav <slavayus@gmail.com>
2018-04-14 19:30:40 +0300

    register 0auth

--- app/src/main/java/com/yandex/gallery/MainActivity.java
+++ app/src/main/java/com/yandex/gallery/MainActivity.java
@@ -1,15 +1,21 @@
 package com.yandex.gallery;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private final String LOG_TAG = this.getClass().getName();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.browser);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(LOG_TAG, " on click");
+                String url = "https://oauth.yandex.ru/authorize?response_type=token&client_id=466db9eb1d744a8b947de4e89551c1ee";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+        });
 
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
