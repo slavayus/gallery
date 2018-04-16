0dfffa7c525c6c57b1d638881a690dd57399ecd7
Vladislav <slavayus@gmail.com>
2018-04-16 12:39:48 +0300

    display elements in RecyclerView

--- app/src/main/java/com/yandex/gallery/ListImagesFragment.java
+++ app/src/main/java/com/yandex/gallery/ListImagesFragment.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by slavik on 4/16/18.
@@ -30,6 +34,61 @@ public class ListImagesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_list_images, container, false);
         mImagesRecyclerView = view.findViewById(R.id.images_recycler_view);
         mImagesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+
         return view;
     }
+
+    private void updateUI() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            data.add(String.valueOf(i));
+        }
+        ImageAdapter imageAdapter = new ImageAdapter(data);
+        mImagesRecyclerView.setAdapter(imageAdapter);
+    }
+
+    private class ImagesHolder extends RecyclerView.ViewHolder {
+        private final TextView mTextView;
+        private String mElement;
+
+        ImagesHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_image, parent, false));
+
+            mTextView = itemView.findViewById(R.id.textView);
+        }
+
+        void bind(String element) {
+            this.mElement = element;
+
+            mTextView.setText(element);
+        }
+    }
+
+    private class ImageAdapter extends RecyclerView.Adapter<ImagesHolder> {
+        private List<String> mData;
+
+        ImageAdapter(List<String> data) {
+            this.mData = data;
+        }
+
+        @Override
+        public ImagesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new ImagesHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ImagesHolder holder, int position) {
+            String value = mData.get(position);
+            holder.bind(value);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+    }
+
 }
