2e271f5aa94904fe676ee39b5818e341e118808a
Vladislav <slavayus@gmail.com>
2018-04-22 01:09:28 +0300

    hide and show toolBar by tapping outside

--- app/src/main/java/com/yandex/gallery/OneImageFragment.java
+++ app/src/main/java/com/yandex/gallery/OneImageFragment.java
@@ -5,6 +5,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +19,10 @@ import com.yandex.gallery.helper.Images;
  */
 
 public class OneImageFragment extends Fragment {
+    private static final String LOG_TAG = "OneImageFragment";
     private Bitmap mImage;
     private static final String IMAGE_INDEX = "image_index";
+    private boolean mIsHidden = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +37,22 @@ public class OneImageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one_image, container, false);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+                if (actionBar != null) {
+                    if (mIsHidden) {
+                        actionBar.show();
+                    } else {
+                        actionBar.hide();
+                    }
+                    mIsHidden = !mIsHidden;
+                }
+            }
+        });
 
         ImageView imageView = view.findViewById(R.id.one_image_view);
         imageView.setImageBitmap(mImage);
