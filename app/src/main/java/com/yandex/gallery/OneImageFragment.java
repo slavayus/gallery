87da809ae974b0cd432baf2b89d05cdf9ddbf161
Vladislav <slavayus@gmail.com>
2018-04-28 10:49:40 +0300

    update layout rotate buttons

--- app/src/main/java/com/yandex/gallery/OneImageFragment.java
+++ app/src/main/java/com/yandex/gallery/OneImageFragment.java
@@ -54,6 +54,7 @@ public class OneImageFragment extends Fragment {
             }
         });
 
+        //TODO: animate rotate buttons
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
